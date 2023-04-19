4b1377479f1ce68f43d5baa9121cd6804c242f97
Andy <78050729+andyhu87@users.noreply.github.com>
2023-04-18 21:35:27 -0400

    Add files via upload

---
+++ 7_Greeting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        DateTime currentTime = DateTime.Now;
+        int currentHour = currentTime.Hour;
+
+        if (currentHour >= 6 && currentHour < 12)
+        {
+            Console.WriteLine("Good Morning");
+        }
+        else if (currentHour >= 12 && currentHour < 17)
+        {
+            Console.WriteLine("Good Afternoon");
+        }
+        else if (currentHour >= 17 && currentHour < 21)
+        {
+            Console.WriteLine("Good Evening");
+        }
+        else
+        {
+            Console.WriteLine("Good Night");
+        }
+
+    }
+}
