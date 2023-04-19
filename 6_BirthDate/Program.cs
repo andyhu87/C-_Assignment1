bc249238e8366cce2c4bb485cab17116fe5f349f
Andy <78050729+andyhu87@users.noreply.github.com>
2023-04-18 21:34:43 -0400

    Add files via upload

---
+++ 6_BirthDate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your birthdate (YYYY-MM-DD): ");
+        DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+        TimeSpan ageInDays = DateTime.Now - birthdate;
+        Console.WriteLine("You are {0} days old.", ageInDays.Days);
+
+        int daysToNextAnniversary = 10000 - (ageInDays.Days % 10000);
+        DateTime nextAnniversary = DateTime.Now.AddDays(daysToNextAnniversary);
+        Console.WriteLine("Your next 10,000 day anniversary is on {0:yyyy-MM-dd}.", nextAnniversary);
+    }
+}
