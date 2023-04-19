9282dfc6b3f69bf89dd95fbf0ac22d0522a0903a
Andy <78050729+andyhu87@users.noreply.github.com>
2023-04-18 21:34:17 -0400

    Add files via upload

---
+++ 5_GuessNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int correctNumber = new Random().Next(3) + 1;
+
+        while (true)
+        {
+            Console.Write("Guess a number between 1 and 3: ");
+            int guessedNumber = int.Parse(Console.ReadLine());
+
+            if (guessedNumber < 1 || guessedNumber > 3)
+            {
+                Console.WriteLine("Invalid guess. Number should be between 1 and 3.");
+                continue;
+            }
+
+            if (guessedNumber < correctNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (guessedNumber > correctNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the correct number!");
+                break;
+            }
+        }
+    }
+}
