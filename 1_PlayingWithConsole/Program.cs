80a94f8fe1a67e56b906ac791940aa3f892e3fd4
Andy <78050729+andyhu87@users.noreply.github.com>
2023-04-18 21:29:16 -0400

    Add files via upload

---
+++ 1_PlayingWithConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("What is your favorite color?");
+        string favoriteColor = Console.ReadLine();
+
+        Console.WriteLine("What is your astrology sign?");
+        string astrologySign = Console.ReadLine();
+
+        Console.WriteLine("What is your street number?");
+        string streetNum = Console.ReadLine();
+
+        string hackerName = $"{favoriteColor}{astrologySign}{streetNum}";
+
+        Console.WriteLine($"Your Hacker Name is {hackerName}.");
+    }
+
+
+}
+
+
